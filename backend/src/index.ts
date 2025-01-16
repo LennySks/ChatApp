3e47eba179e8731a50a5cd97301353629bf83002
dxvee <david.pietrzak@student.kdg.be>
2025-01-16 15:12:10 +0100

    docker compose file for mongodb + mongo express + signup controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import {router} from "./routes/auth.route";
+import {connectDB} from "./lib/db";
 
 dotenv.config();
 const app: Express = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+
+app.use(express.json());
+
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
+    connectDB();
 })
+
+
+app.use("/api/auth", router)
